821d78af0da316d409005198e94e014e26b8a9a9
Joshoa <marcos.parentes@afya.com.br>
2024-05-02 16:09:20 -0300

    Initial Commit

---
+++ MjopPlayer/MjopPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MjopPlayerApp.swift
+//  MjopPlayer
+//
+//  Created by Marcos Joshoa on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MjopPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
